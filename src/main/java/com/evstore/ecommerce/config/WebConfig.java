f99a4e158450099a1a6879c5bb9ac4a92c059bc9
yeolmoo <hongseu4@gmail.com>
2025-04-04 23:57:07 -0400

    fix config

--- src/main/java/com/evstore/ecommerce/config/WebConfig.java
+++ src/main/java/com/evstore/ecommerce/config/WebConfig.java
@@ -19,25 +19,25 @@
 //    }
 //}
 
-package com.evstore.ecommerce.config;
+// package com.evstore.ecommerce.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:3000",
-                    "http://localhost:3002",
-                    "https://eecs-evstore.netlify.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins(
+//                     "http://localhost:3000",
+//                     "http://localhost:3002",
+//                     "https://eecs-evstore.netlify.app"
+//                 )
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+//     }
+// }
